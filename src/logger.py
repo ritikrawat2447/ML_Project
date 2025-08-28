d857b9628f9614f4a1455bd48b0199ef9e3572ca
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2025-08-28 21:50:54 +0530

    EDA and Problem Statement

--- src/logger.py
+++ src/logger.py
@@ -14,5 +14,5 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-if __name__ == "__main__":
-    logging.info("Loggin has started")
+# if __name__ == "__main__":
+#     logging.info("Loggin has started")
